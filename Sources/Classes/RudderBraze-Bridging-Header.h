de495bfbee621eef3f2863124c8d4b7a4b75a0a3
Roscoe Evans <revans@acorns.com>
2024-09-25 16:03:55 -0600

    made SPM compatible

---
+++ Sources/Classes/RudderBraze-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  RudderBraze
+//
+//  Created by Roscoe Evans on 9/23/24.
+//
+
+#import <Rudder/Rudder.h>
--- Sources/Classes/RudderBrazeIntegration.h
+++ Sources/Classes/RudderBrazeIntegration.h
@@ -7,8 +7,8 @@
 #import <Foundation/Foundation.h>
 
 @import BrazeKit;
-#import <Rudder/Rudder.h>
- 
+#import "Rudder.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface BrazePurchase : NSObject
